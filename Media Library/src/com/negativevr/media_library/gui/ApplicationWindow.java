ef042b8502abe78d90e7349a9a9ffc01173e1b9d
Ryan May <xtheraingoddessx@gmail.com>
2017-05-23 12:49:18 -0700

    started creating framework for data submission in ApplicationWindow

--- Media Library/src/com/negativevr/media_library/gui/ApplicationWindow.java
+++ Media Library/src/com/negativevr/media_library/gui/ApplicationWindow.java
@@ -8,8 +8,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -26,7 +28,7 @@ public class ApplicationWindow extends Application{
 	@Override
 	public void start(Stage rootStage) throws Exception { 
 		VBox componentLayout = new VBox();
-		componentLayout.setPadding(new Insets(20,0,20,20));
+		componentLayout.setPadding(new Insets(0,0,20,0));
 		
         //The button uses an inner class to handle the button click event
         Button tmpButton = new Button("Button");
@@ -37,9 +39,10 @@ public class ApplicationWindow extends Application{
             }
         });
         
-        //Add the BorderPane to the Scene
+        //Add the VBox to the Scene
         Scene scene = new Scene(componentLayout,500,500);
         
+        //Adds MenuBar to all children Views
         ((VBox) scene.getRoot()).getChildren().addAll(setupMenuBar());
         
         //Add the Scene to the Stage
@@ -108,6 +111,43 @@ public class ApplicationWindow extends Application{
 //private button mutators
 	private void showAddWindow(){
 		System.out.println("HI");
+		Stage addWindow = new Stage();
+		addWindow.setTitle("ADD Media");
+		GridPane componentLayout = new GridPane();
+		componentLayout.setPadding(new Insets(10, 10, 10, 10));
+		componentLayout.setVgap(5);
+		componentLayout.setHgap(5);
+		
+		//Defining the Name text field
+		final TextField name = new TextField();
+		name.setPromptText("Enter your first name.");
+		name.setPrefColumnCount(10);
+		name.getText();
+		GridPane.setConstraints(name, 0, 0);
+		componentLayout.getChildren().add(name);
+	
+		//Defining the Last Name text field
+		final TextField lastName = new TextField();
+		lastName.setPromptText("Enter your last name.");
+		GridPane.setConstraints(lastName, 0, 1);
+		componentLayout.getChildren().add(lastName);
+		//Defining the Comment text field
+		final TextField comment = new TextField();
+		comment.setPrefColumnCount(15);
+		comment.setPromptText("Enter your comment.");
+		GridPane.setConstraints(comment, 0, 2);
+		componentLayout.getChildren().add(comment);
+		//Defining the Submit button
+		Button submit = new Button("Submit");
+		GridPane.setConstraints(submit, 1, 0);
+		componentLayout.getChildren().add(submit);
+		//Defining the Clear button
+		Button clear = new Button("Clear");
+		GridPane.setConstraints(clear, 1, 1);
+		componentLayout.getChildren().add(clear);
+		Scene scene = new Scene(componentLayout,400,400);
+		addWindow.setScene(scene);
+		addWindow.show();
 	}
 	
 	private void showRemoveWindow(){
