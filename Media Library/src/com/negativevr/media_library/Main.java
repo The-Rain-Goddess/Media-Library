c1705c3755c87147932b2a52b464780663a091bb
Ryan May <xtheraingoddessx@gmail.com>
2017-05-24 12:17:17 -0700

    added master data map and utility methods for it

--- Media Library/src/com/negativevr/media_library/Main.java
+++ Media Library/src/com/negativevr/media_library/Main.java
@@ -1,12 +1,22 @@
 package com.negativevr.media_library;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.negativevr.media_library.files.MediaFile;
+
 /**
  * 
- * @author Vaneh
+ * @author Vaneh Boghosian, Ryan May
  *
  */
 
 public class Main {
-
+	private static final Map<Long, MediaFile> masterMediaData = Collections.synchronizedMap(new TreeMap<Long, MediaFile>());
+	
 	public Main() {
 		// TODO Auto-generated constructor stub
 	}
@@ -14,7 +24,19 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
 	}
-
+	
+//non-private accessors / mutators	
+	/**
+	 * Returns the Master Media Data map; a synchronized TreeMap<Long, {@link com.negativevr.media_library.files.MediaFile;}>.
+	 * @return masterMediaData
+	 */
+	public static Map<Long, MediaFile> getMasterData(){ return masterMediaData; }
+	
+	public static List<MediaFile> getMasterDataAsList(){
+		List<MediaFile> list = new ArrayList<>();
+		for( Map.Entry<Long, MediaFile> entry : masterMediaData.entrySet()){
+			list.add(entry.getValue());
+		} return list;
+	}
 }
