7dd3c52e99e8cec1603f158c4a995f4ec8290dc7
Ryan May <xtheraingoddessx@gmail.com>
2017-05-24 10:26:30 -0700

    added Labels and path selection buttons to add window in AppWindow

--- Media Library/src/com/negativevr/media_library/gui/ApplicationWindow.java
+++ Media Library/src/com/negativevr/media_library/gui/ApplicationWindow.java
@@ -1,4 +1,5 @@
 package com.negativevr.media_library.gui;
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,6 +10,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Control;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -17,6 +19,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class ApplicationWindow extends Application{
@@ -44,7 +47,7 @@ public class ApplicationWindow extends Application{
         });
         
         //Add the VBox to the Scene
-        Scene scene = new Scene(componentLayout,500,500);
+        Scene scene = new Scene(componentLayout,600,600);
         
         //Adds MenuBar to all children Views
         ((VBox) scene.getRoot()).getChildren().addAll(setupMenuBar());
@@ -119,9 +122,9 @@ public class ApplicationWindow extends Application{
 		addWindow.setTitle("ADD Media");
 		GridPane componentLayout = getAddWindowLayout();
 		
-		componentLayout.getChildren().addAll(getWindowComponents());
+		componentLayout.getChildren().addAll(getWindowComponents(addWindow));
 		
-		Scene scene = new Scene(componentLayout,400,400);
+		Scene scene = new Scene(componentLayout,550,300);
 		addWindow.setScene(scene);
 		addWindow.show();
 	}
@@ -134,34 +137,88 @@ public class ApplicationWindow extends Application{
 		return componentLayout;
 	}
 	
-	private List<Control> getWindowComponents(){
+	private List<Control> getWindowComponents(Stage parent){
+		final FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Media Selection.");
+		
+		//Defining path text field
+		final TextField songPath = new TextField();
+		songPath.setPromptText("C:\\");
+		songPath.setPrefColumnCount(30);
+		songPath.setEditable(false);
+		songPath.setFocusTraversable(false);
+		GridPane.setConstraints(songPath, 1, 0);
+		final Label songPathLabel = new Label("Path: ");
+		GridPane.setConstraints(songPathLabel, 0, 0);
+				
 		//Defining the Name text field
-		final TextField name = new TextField();
-		name.setPromptText("Enter your first name.");
-		name.setPrefColumnCount(10);
-		name.getText();
-		GridPane.setConstraints(name, 0, 0);
+		final TextField songName = new TextField();
+		songName.setPromptText("Enter the song's name. *");
+		songName.setPrefColumnCount(20);
+		GridPane.setConstraints(songName, 1, 2);
+		final Label songNameLabel = new Label("Name: ");
+		GridPane.setConstraints(songNameLabel, 0, 2);
 
 		//Defining the Last Name text field
-		final TextField lastName = new TextField();
-		lastName.setPromptText("Enter your last name.");
-		GridPane.setConstraints(lastName, 0, 1);
+		final TextField artistNames = new TextField();
+		artistNames.setPromptText("Enter the artist name(s). *");
+		GridPane.setConstraints(artistNames, 1, 3);
+		final Label songArtistLabel = new Label("Artist(s): ");
+		GridPane.setConstraints(songArtistLabel, 0, 3);
 		
 		//Defining the Comment text field
-		final TextField comment = new TextField();
-		comment.setPrefColumnCount(15);
-		comment.setPromptText("Enter your comment.");
-		GridPane.setConstraints(comment, 0, 2);
+		final TextField albumName = new TextField();
+		albumName.setPrefColumnCount(15);
+		albumName.setPromptText("Enter the album name. *");
+		GridPane.setConstraints(albumName, 1, 4);
+		final Label songAlbumLabel = new Label("Album: ");
+		GridPane.setConstraints(songAlbumLabel, 0, 4);
+		
+		//album number
+		final TextField albumNumber = new TextField();
+		albumNumber.setPrefColumnCount(20);
+		albumNumber.setPromptText("Enter the song's album number.");
+		GridPane.setConstraints(albumNumber, 1, 5);
+		final Label songNumberLabel = new Label("Number: ");
+		GridPane.setConstraints(songNumberLabel, 0, 5);
+		
+		//genre
+		final TextField genre = new TextField();
+		genre.setPrefColumnCount(20);
+		genre.setPromptText("Enter the song's genre.");
+		GridPane.setConstraints(genre, 1, 6);
+		final Label songGenreLabel = new Label("Genre: ");
+		GridPane.setConstraints(songGenreLabel, 0, 6);
 		
 		//Defining the Submit button
 		Button submit = new Button("Submit");
-		GridPane.setConstraints(submit, 1, 0);
+		GridPane.setConstraints(submit, 2, 3);
 		
 		//Defining the Clear button
 		Button clear = new Button("Clear");
-		GridPane.setConstraints(clear, 1, 1);
+		GridPane.setConstraints(clear, 2, 4);
 		
-		return Arrays.asList(name, lastName, comment, submit, clear);
+		//Open file button
+		final Button openFile = new Button("Add media files...");
+		openFile.setOnAction(
+	            new EventHandler<ActionEvent>() {
+	            	final TextField path = songPath;
+	                @Override
+	                public void handle(final ActionEvent e) {
+	                    List<File> list =
+	                        fileChooser.showOpenMultipleDialog(parent);
+	                    if (list != null) {
+	                        for (File file : list) {
+	                            path.appendText(file.getAbsolutePath().toString() + "; ");
+	                        }
+	                    }
+	                }
+	            });
+		GridPane.setConstraints(openFile, 2, 0);
+		
+		return Arrays.asList(	songPathLabel, songNameLabel, songArtistLabel, songAlbumLabel, songNumberLabel, songGenreLabel,
+								songPath, songName, artistNames, albumName, albumNumber, genre,
+								openFile, submit, clear);
 	}
 	
 	private void showRemoveWindow(){
