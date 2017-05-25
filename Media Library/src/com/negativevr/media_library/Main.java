2139433b582b254beedb561fa464e7fcd93d1be2
Ryan May <xtheraingoddessx@gmail.com>
2017-05-24 22:10:51 -0700

    added more test cases

--- Media Library/src/com/negativevr/media_library/Main.java
+++ Media Library/src/com/negativevr/media_library/Main.java
@@ -1,6 +1,7 @@
 package com.negativevr.media_library;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -8,6 +9,8 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import com.negativevr.media_library.files.MediaFile;
+import com.negativevr.media_library.files.MediaFileAttribute;
+import com.negativevr.media_library.gui.ApplicationWindow;
 
 /**
  * 
@@ -26,23 +29,39 @@ public class Main {
 		List<String> name= new ArrayList<>();
 		name.add("The Killers");
 		MediaFile file = new MediaFile(name, "Kill\n", 4, new Date().toString(),
-				 new Date().toString(), "When You Were Young\n", 210.0, "C:\\", 124L);
+				 new Date().toString(), "When You Were Young\n", 210.0, "C:\\","Alternative", 124L);
 		
 		List<String> name1= new ArrayList<>();
 		name1.add("One Republic");
 		MediaFile file1 = new MediaFile(name1, "Hi\n", 6, new Date().toString(),
-				 new Date().toString(), "All The Right Moves\n", 193.0, "C:\\", 3245L);
+				 new Date().toString(), "All The Right Moves\n", 193.0, "C:\\", "Rock", 3245L);
 		
 		List<String> name2= new ArrayList<>();
 		name2.add("Stromae");
 		MediaFile file2 = new MediaFile(name2, "Flower\n", 12, new Date().toString(),
-				 new Date().toString(), "Formidable\n", 510.0, "C:\\", 1214L);
+				 new Date().toString(), "Formidable\n", 510.0, "C:\\","Pop", 1214L);
+		
+		MediaFile file3 = new MediaFile(new MediaFileAttribute().setArtistStrings(Arrays.asList("The Killers"))
+										.setAlbum("Sam's Town")
+										.setName("Jenny Was A Friend Of Mine")
+										.setGenre("Indie Rock")
+										.setDateCreated(new Date().toString())
+										.setDateRecorded("9/5/2006")
+										.setLength(192)
+										.setNumber(1)
+										.setPath("C:\\")
+										.setPlays(0)
+				, 1000L);
 		
 		masterMediaData.put(124L, file);
 		masterMediaData.put(3245L, file1);
 		masterMediaData.put(1214L, file2);
+		masterMediaData.put(file3.getUUID(), file3);
 		 
 		System.out.println(masterMediaData);
+		
+		ApplicationWindow app = new ApplicationWindow();
+		app.begin(args);
 	}
 	
 //non-private accessors / mutators	
