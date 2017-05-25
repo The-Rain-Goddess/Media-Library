4ed20e4de4642cd3978cc3daae9050799c8801e5
Ryan May <xtheraingoddessx@gmail.com>
2017-05-24 17:50:45 -0700

    added test cases to Main

--- Media Library/src/com/negativevr/media_library/Main.java
+++ Media Library/src/com/negativevr/media_library/Main.java
@@ -2,6 +2,7 @@ package com.negativevr.media_library;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -22,7 +23,27 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
+		List<String> name= new ArrayList<>();
+		name.add("The Killers");
+		MediaFile file = new MediaFile(name, "Kill\n", 4, new Date().toString(),
+				 new Date().toString(), "When You Were Young\n", 3.14, "C:\\", 124L);
 		
+		List<String> name1= new ArrayList<>();
+		name1.add("One Republic");
+		MediaFile file1 = new MediaFile(name1, "Hi\n", 6, new Date().toString(),
+				 new Date().toString(), "All The Right Moves\n", 2.33, "C:\\", 3245L);
+		
+		List<String> name2= new ArrayList<>();
+		name2.add("Stromae");
+		MediaFile file2 = new MediaFile(name2, "Flower\n", 12, new Date().toString(),
+				 new Date().toString(), "Formidable\n", 5.10, "C:\\", 1214L);
+		
+		 Map<Integer, MediaFile> map = new TreeMap<Integer, MediaFile>();  
+		map. put(124, file);
+		map. put(3245, file1);
+		map. put(1214, file2);
+		 
+		System.out.println(map);
 	}
 	
 //non-private accessors / mutators	
