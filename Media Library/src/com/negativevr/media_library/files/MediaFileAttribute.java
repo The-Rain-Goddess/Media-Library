2613cde4172aeb074af3196bc7ad5104646538dd
Ryan May <xtheraingoddessx@gmail.com>
2017-05-24 22:13:02 -0700

    added additional get methods

--- Media Library/src/com/negativevr/media_library/files/MediaFileAttribute.java
+++ Media Library/src/com/negativevr/media_library/files/MediaFileAttribute.java
@@ -1,9 +1,15 @@
 package com.negativevr.media_library.files;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class MediaFileAttribute {
@@ -12,6 +18,20 @@ public class MediaFileAttribute {
 	private IntegerProperty number;
 	private DoubleProperty length;
 	private IntegerProperty plays;
+	
+//contructor
+	public MediaFileAttribute(){
+		this.name = new SimpleStringProperty("");
+		this.album = new SimpleStringProperty("");
+		this.dateCreated = new SimpleStringProperty(new Date().toString());
+		this.dateRecorded = new SimpleStringProperty("");
+		this.path = new SimpleStringProperty("C:\\");
+		this.genre = new SimpleStringProperty("");
+		this.artists = Arrays.asList(new SimpleStringProperty(""));
+		this.number = new SimpleIntegerProperty(0);
+		this.plays = new SimpleIntegerProperty(0);
+		this.length = new SimpleDoubleProperty(0.0);
+	}
 
 //getters	
 	/**
@@ -93,6 +113,15 @@ public class MediaFileAttribute {
 		this.name = name;
 		return this;
 	}
+	
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public MediaFileAttribute setName(String name) {
+		this.name = new SimpleStringProperty(name);
+		return this;
+	}
 
 	/**
 	 * @param album
@@ -100,6 +129,15 @@ public class MediaFileAttribute {
 	 */
 	public MediaFileAttribute setAlbum(StringProperty album) {
 		this.album = album;
+		return this;
+	}
+	
+	/**
+	 * @param album
+	 *            the album to set
+	 */
+	public MediaFileAttribute setAlbum(String album) {
+		this.album = new SimpleStringProperty(album);
 		return this;
 	}
 
@@ -111,6 +149,15 @@ public class MediaFileAttribute {
 		this.dateCreated = dateCreated;
 		return this;
 	}
+	
+	/**
+	 * @param dateCreated
+	 *            the dateCreated to set
+	 */
+	public MediaFileAttribute setDateCreated(String dateCreated) {
+		this.dateCreated = new SimpleStringProperty(dateCreated);
+		return this;
+	}
 
 	/**
 	 * @param dateRecorded
@@ -118,6 +165,15 @@ public class MediaFileAttribute {
 	 */
 	public MediaFileAttribute setDateRecorded(StringProperty dateRecorded) {
 		this.dateRecorded = dateRecorded;
+		return this;
+	}
+	
+	/**
+	 * @param dateRecorded
+	 *            the dateRecorded to set
+	 */
+	public MediaFileAttribute setDateRecorded(String dateRecorded) {
+		this.dateRecorded = new SimpleStringProperty(dateRecorded);
 		return this;
 	}
 
@@ -129,6 +185,15 @@ public class MediaFileAttribute {
 		this.path = path;
 		return this;
 	}
+	
+	/**
+	 * @param path
+	 *            the path to set
+	 */
+	public MediaFileAttribute setPath(String path) {
+		this.path = new SimpleStringProperty(path);
+		return this;
+	}
 
 	/**
 	 * @param genre
@@ -136,6 +201,15 @@ public class MediaFileAttribute {
 	 */
 	public MediaFileAttribute setGenre(StringProperty genre) {
 		this.genre = genre;
+		return this;
+	}
+	
+	/**
+	 * @param genre
+	 *            the genre to set
+	 */
+	public MediaFileAttribute setGenre(String genre) {
+		this.genre = new SimpleStringProperty(genre);
 		return this;
 	}
 
@@ -147,6 +221,18 @@ public class MediaFileAttribute {
 		this.artists = artists;
 		return this;
 	}
+	
+	/**
+	 * @param artists
+	 *            the artists to set
+	 */
+	public MediaFileAttribute setArtistStrings(List<String> artists) {
+		List<StringProperty> list = new ArrayList<>();
+		for(String s : artists){
+			list.add(new SimpleStringProperty(s));
+		} this.artists = list;
+		return this;
+	}
 
 	/**
 	 * @param number
@@ -154,6 +240,15 @@ public class MediaFileAttribute {
 	 */
 	public MediaFileAttribute setNumber(IntegerProperty number) {
 		this.number = number;
+		return this;
+	}
+	
+	/**
+	 * @param number
+	 *            the number to set
+	 */
+	public MediaFileAttribute setNumber(int number) {
+		this.number = new SimpleIntegerProperty(number);
 		return this;
 	}
 
@@ -165,6 +260,15 @@ public class MediaFileAttribute {
 		this.length = length;
 		return this;
 	}
+	
+	/**
+	 * @param length
+	 *            the length to set
+	 */
+	public MediaFileAttribute setLength(double length) {
+		this.length = new SimpleDoubleProperty(length);
+		return this;
+	}
 
 	/**
 	 * @param plays
@@ -172,6 +276,15 @@ public class MediaFileAttribute {
 	 */
 	public MediaFileAttribute setPlays(IntegerProperty plays) {
 		this.plays = plays;
+		return this;
+	}
+	
+	/**
+	 * @param plays
+	 *            the plays to set
+	 */
+	public MediaFileAttribute setPlays(int plays) {
+		this.plays = new SimpleIntegerProperty(plays);
 		return this;
 	}
 }
