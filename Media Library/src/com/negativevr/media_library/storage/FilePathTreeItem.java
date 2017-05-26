ad7f826ba073b293592fa23e08e5fe94284956cb
Ryan May <xtheraingoddessx@gmail.com>
2017-05-25 18:20:19 -0700

    added Override to method isLeaf to allow auto dir opening

--- Media Library/src/com/negativevr/media_library/storage/FilePathTreeItem.java
+++ Media Library/src/com/negativevr/media_library/storage/FilePathTreeItem.java
@@ -9,87 +9,100 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 
-import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.TreeItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class FilePathTreeItem extends TreeItem<String> {
-	public static Image folderCollapseImage=new Image(ClassLoader.getSystemResourceAsStream("add.png"));
-    public static Image folderExpandImage=new Image(ClassLoader.getSystemResourceAsStream("add.png"));
-    public static Image fileImage=new Image(ClassLoader.getSystemResourceAsStream("add.png"));
-  
-    //this stores the full path to the file or directory
-    private String fullPath;
-    public String getFullPath(){return(this.fullPath);}
-  
-    private boolean isDirectory;
-    public boolean isDirectory(){return(this.isDirectory);}
-    
-    @SuppressWarnings("unchecked")
-	public FilePathTreeItem(Path file){
-    	super(file.toString());
-    	this.fullPath=file.toString();
-    	//test if this is a directory and set the icon
-        if(Files.isDirectory(file)){
-          this.isDirectory=true;
-          this.setGraphic(new ImageView(folderCollapseImage));
-        }else{
-          this.isDirectory=false;
-          this.setGraphic(new ImageView(fileImage));
-          //if you want different icons for different file types this is where you'd do it
-        }
-        
-        //set the value
-        if(!fullPath.endsWith(File.separator)){
-          //set the value (which is what is displayed in the tree)
-          String value=file.toString();
-          int indexOf=value.lastIndexOf(File.separator);
-          if(indexOf>0){
-            this.setValue(value.substring(indexOf+1));
-          }else{
-            this.setValue(value);
-          }
-        }
-        
-        this.addEventHandler(TreeItem.branchExpandedEvent(),new EventHandler(){
-            @Override
-            public void handle(Event e){
-              FilePathTreeItem source=(FilePathTreeItem)e.getSource();
-              if(source.isDirectory()&&source.isExpanded()){
-                ImageView iv=(ImageView)source.getGraphic();
-                iv.setImage(folderExpandImage);
-              }
-              try{
-                if(source.getChildren().isEmpty()){
-                  Path path=Paths.get(source.getFullPath());
-                  BasicFileAttributes attribs=Files.readAttributes(path,BasicFileAttributes.class);
-                  if(attribs.isDirectory()){
-                    DirectoryStream<Path> dir=Files.newDirectoryStream(path);
-                    for(Path file:dir){
-                      FilePathTreeItem treeNode=new FilePathTreeItem(file);
-                      source.getChildren().add(treeNode);
-                    }
-                  }
-                }else{
-                  //if you want to implement rescanning a directory for changes this would be the place to do it
-                }
-              }catch(IOException x){
-                x.printStackTrace();
-              }
-            }
-          });
-        
-        this.addEventHandler(TreeItem.branchCollapsedEvent(),new EventHandler(){
-            @Override
-            public void handle(Event e){
-              FilePathTreeItem source=(FilePathTreeItem)e.getSource();
-              if(source.isDirectory()&&!source.isExpanded()){
-                ImageView iv=(ImageView)source.getGraphic();
-                iv.setImage(folderCollapseImage);
-              }
-            }
-          });
-    }    
+	public static Image folderCollapseImage = new Image(ClassLoader.getSystemResourceAsStream("folder.png"));
+	public static Image folderExpandImage = new Image(ClassLoader.getSystemResourceAsStream("open folder.png"));
+	public static Image fileImage = new Image(ClassLoader.getSystemResourceAsStream("document.png"));
+
+	// this stores the full path to the file or directory
+	private String fullPath;
+
+	public String getFullPath() {
+		return (this.fullPath);
+	}
+
+	private boolean isDirectory;
+
+	public boolean isDirectory() {
+		return (this.isDirectory);
+	}
+
+	public FilePathTreeItem(Path file) {
+		super(file.toString());
+		this.fullPath = file.toString();
+
+		// test if this is a directory and set the icon
+		if (Files.isDirectory(file)) {
+			this.isDirectory = true;
+			this.setGraphic(new ImageView(folderCollapseImage));
+		} else {
+			this.isDirectory = false;
+			this.setGraphic(new ImageView(fileImage));
+			// if you want different icons for different file types this is
+			// where you'd do it
+		}
+
+		// set the value
+		if (!fullPath.endsWith(File.separator)) {
+			// set the value (which is what is displayed in the tree)
+			String value = file.toString();
+			int indexOf = value.lastIndexOf(File.separator);
+			if (indexOf > 0) {
+				this.setValue(value.substring(indexOf + 1));
+			} else {
+				this.setValue(value);
+			}
+		}
+
+		this.addEventHandler(TreeItem.branchExpandedEvent(), new EventHandler<TreeItem.TreeModificationEvent<String>>() {
+			@Override
+			public void handle(TreeModificationEvent<String> e) {
+				FilePathTreeItem source = (FilePathTreeItem) e.getSource();
+				if (source.isDirectory() && source.isExpanded()) {
+					ImageView iv = (ImageView) source.getGraphic();
+					iv.setImage(folderExpandImage);
+				} try {
+					if (source.getChildren().isEmpty()) {
+						Path path = Paths.get(source.getFullPath());
+						BasicFileAttributes attribs = Files.readAttributes(path, BasicFileAttributes.class);
+						if (attribs.isDirectory()) {
+							DirectoryStream<Path> dir = Files.newDirectoryStream(path);
+							for (Path file : dir) {
+								FilePathTreeItem treeNode = new FilePathTreeItem(file);
+								source.getChildren().add(treeNode);
+							}
+						}
+					} else {
+						// if you want to implement rescanning a directory for
+						// changes this would be the place to do it
+					}
+				} catch (IOException x) {
+					x.printStackTrace();
+				}
+			}
+		});
+
+		this.addEventHandler(TreeItem.branchCollapsedEvent(), new EventHandler<TreeItem.TreeModificationEvent<String>>() {
+			@Override
+			public void handle(TreeModificationEvent<String> e) {
+				FilePathTreeItem source = (FilePathTreeItem) e.getSource();
+				if (source.isDirectory() && !source.isExpanded()) {
+					ImageView iv = (ImageView) source.getGraphic();
+					iv.setImage(folderCollapseImage);
+				}
+			}
+		});
+	}
+	
+	@Override
+	public boolean isLeaf(){
+		if(isDirectory)
+			return false;
+		else return true;
+	}
 }
