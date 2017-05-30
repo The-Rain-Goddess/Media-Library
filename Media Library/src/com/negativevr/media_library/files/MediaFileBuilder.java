9920e0cc994579cd19f089cf057abc829bbd2fc3
Ryan May <xtheraingoddessx@gmail.com>
2017-05-29 20:42:18 -0700

    added getters and setters for the MediaFile builder

--- Media Library/src/com/negativevr/media_library/files/MediaFileBuilder.java
+++ Media Library/src/com/negativevr/media_library/files/MediaFileBuilder.java
@@ -4,6 +4,20 @@ public class MediaFileBuilder {
 	private MediaFile file;
 	
 	public MediaFileBuilder(){
-		file = new MediaFile();
+		setFile(new MediaFile());
+	}
+
+	/**
+	 * @return the file
+	 */
+	public MediaFile getFile() {
+		return file;
+	}
+
+	/**
+	 * @param file the file to set
+	 */
+	public void setFile(MediaFile file) {
+		this.file = file;
 	}
 }
