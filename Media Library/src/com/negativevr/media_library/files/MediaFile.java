883b421c0e151e40c1ebd227a12ecfb80bf13285
Ryan May <xtheraingoddessx@gmail.com>
2017-06-03 13:20:39 -0700

    added javadoc author tags

--- Media Library/src/com/negativevr/media_library/files/MediaFile.java
+++ Media Library/src/com/negativevr/media_library/files/MediaFile.java
@@ -12,6 +12,12 @@ import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+
+/**
+ * 
+ * @author Vaneh Boghosian, Ryan May
+ *
+ */
  
 public class MediaFile implements Serializable {
 
